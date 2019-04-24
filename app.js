20a212a7896df7f5f66d516b7a43170a79dd0ca4
hirwavestine <gilhirwa22@gmail.com>
2019-04-24 10:18:27 +0200

    setting up home page

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+const exphbs = require("express-handlebars");
+
+//-dirname,"public":I am whatever for there I am or whatever file I am is going
+//to include that way if or basically joined together with the public
+app.use(express.static(path.join(__dirname, "public")));
+app.engine("handlebars", exphbs({ defaultLayout: "home" }));
+app.set("view engine", "handlebars");
+
+app.get("/", (req, res) => {
+  res.render("home/index");
+});
 
 app.listen(4500, () => {
   console.log(`listen on port 4500`);
