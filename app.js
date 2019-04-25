1e0acfb134a9d242d3a515aae86b5b549e4ebd88
hirwavestine <gilhirwa22@gmail.com>
2019-04-25 14:03:40 +0200

    add login and registration views

--- app.js
+++ app.js
@@ -15,6 +15,12 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
   res.render("home/about");
 });
+app.get("/login", (req, res) => {
+  res.render("home/login");
+});
+app.get("/register", (req, res) => {
+  res.render("home/register");
+});
 app.listen(4500, () => {
   console.log(`listen on port 4500`);
 });
