aebbb0be9486ffe2319efcab0c48c3eb0781574e
hirwavestine <gilhirwa22@gmail.com>
2019-04-30 09:35:36 +0200

    creating post routes

--- app.js
+++ app.js
@@ -10,10 +10,12 @@ app.set("view engine", "handlebars");
 //load all those routes
 const home = require("./routes/home/index");
 const admin = require("./routes/admin/index");
+const posts = require("./routes/admin/posts");
 //plug things in our application, here we use middleware, Use routes
 //("/admin", admin) this means that any code reach slash which is the route admin is going to execute admin
 app.use("/", home);
 app.use("/admin", admin);
+app.use("/admin/posts", posts);
 app.listen(4500, () => {
   console.log(`listen on port 4500`);
 });
