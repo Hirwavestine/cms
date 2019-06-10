a95287350ecd9707a46a0576f5fe913340761cb5
hirwavestine <gilhirwa22@gmail.com>
2019-06-10 15:25:34 +0200

    Dummy data creation finished

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const faker = require("faker");
+const Post = require("../../models/Post");
 
 // router.all("/*":Ok listen I want to have everything that's in after admin and I want somethiing to happen to it.bcse by default we set defaultLayout: "home" in app.js
 router.all("/*", (req, res, next) => {
@@ -18,4 +20,20 @@ router.get("/", (req, res) => {
 // router.get("/charts", (req, res) => {
 //   res.render("admin/charts");
 // });
+router.post("/generate-fake-posts", (req, res) => {
+  // res.send("It works");
+  for (let i = 0; i < req.body.amount; i++) {
+    let post = new Post();
+    post.title = faker.name.title();
+    post.status = "public";
+    post.allowComments = faker.random.boolean();
+    post.body = faker.lorem.sentence();
+    post.slug = faker.name.title();
+    post.save(function(err) {
+      if (err) throw err;
+    });
+  }
+  res.redirect("/admin/posts");
+});
+
 module.exports = router;
